4cb3b77d4d1b164cef043842558a95d832a7009a
markiewaters <darkmark53@gmail.com>
2023-09-17 14:46:09 -0400

    Adding Week 4 HomeWork

---
+++ Week4Homework/index.js
@@ -0,0 +1,4 @@
+// Creating the DOM to be mutated
+const container = document.querySelector(".container");
+const heading1 = document.createElement("h1");
+const paragraph = document.createElement("p");
